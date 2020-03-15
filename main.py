404c99d2e3dfacd1d5aceba8a1328804d5025079
Juanjo Salvador <juanjosalvador@netc.eu>
2020-03-15 19:00:22 +0100

    New report format

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
 urllib3.disable_warnings()
 
-COUNTRY = "spain" # Change this!
+COUNTRY = "Spain" # Change this!
 
 def tg_send(message):
     URL = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(
@@ -37,22 +37,38 @@ def tg_delete(message_id):
     except Exception as ex:
         print("Something went wrong: \n {}".format(ex))
 
-def main():
-    # Get the data
-    r = requests.get("https://www.worldometers.info/coronavirus/country/{}/".format(COUNTRY))
+def get_data():
+    r = requests.get("https://www.worldometers.info/coronavirus/")#.format(COUNTRY))
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    covid_raw = soup.select("div.maincounter-number > span")
+    #covid_raw = soup.select("#main_table_countries > tbody:nth-child(2)")
+    rows = soup.find_all('tr')
+
+    parsed_data = []
+
+    for row in rows:
+        cols = row.find_all('td')
+        for col in cols:
+            if col.text.strip() == COUNTRY.capitalize():
+                country_data = row.find_all('td')
+                for c_data in country_data:
+                    parsed_data.append(c_data.text.lstrip().replace("+", ""))
+
+    return parsed_data
+
+def main():
+    
+    covid_data = get_data()
 
     message = "Informe del {} \n\n\
-    * {}casos confirmados en España.\n\
-    * {} recuperados.\n\
-    * {} muertos.\n\n\
-    Recuerda, es importante quedarse en casa, lavarse las manos \
-    con regularidad y en caso de síntomas, acudir al teléfono \
-    habilitado por las autoridades sanitarias de tu comunidad.".format(
+* {} casos confirmados en España ({} hoy).\n\
+* {} recuperados.\n\
+* {} muertos ({} hoy).\n\n\
+Recuerda, es importante quedarse en casa, lavarse las manos \
+con regularidad y en caso de síntomas, acudir al teléfono \
+habilitado por las autoridades sanitarias de tu comunidad.".format(
         dt.now().strftime("%d/%m/%Y a las %H:%M:%S"),
-        covid_raw[0].text, covid_raw[2].text, covid_raw[1].text)
+        covid_data[1], covid_data[2], covid_data[5], covid_data[3], covid_data[4])
 
     if config['DEFAULT'].getboolean('MASTODON_ENABLED'):
         # Mastodon config
@@ -73,7 +89,7 @@ def main():
                 tg_delete(last_message)
             except FileNotFoundError as err:
                 pass
-            
+
             log = open('message_id.log', 'w')
             log.write(str(response['result']['message_id']))
             log.close()
